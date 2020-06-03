1622f59efffdb3878b148f9ea5782d0ad004c55a
Vetri Velan <velanvetri5@gmail.com>
2020-06-02 23:07:19 -0700

    Text ready for left half

--- XenimationConfig.py
+++ XenimationConfig.py
@@ -1,11 +1,15 @@
 import numpy as np
 import math as m
 
+
+
+##### USER MIGHT NEED TO CHANGE THESE #####
+
 # Figure dimensions
-fig_width = 15
-fig_height = 10
+fig_width = 16
+fig_height = 9
 axes_xmax = 1. * fig_width
-axes_ymin = -1. * fig_width
+axes_ymin = -1. * fig_height
 
 # Colors
 NR_color = '#FF0000'
@@ -16,13 +20,17 @@ EDep_color = '#6AA84F'
 NR_heat_fraction = 0.85
 ER_heat_fraction = 0.27
 
-# Arrow positions, angles, lengths, and widths (width for a0 only)
+
+
+##### USER UNLIKELY TO NEED TO CHANGE THESE #####
+
+# Arrow box positions, angles, lengths, and widths (width for a0 only)
 # xr means x-relative, i.e. relative to (0,0) at top-left and (1, -1) at bottom-right
-a0_eDep_xr = 0.14
-a0_eDep_yr = 0.49
+a0_eDep_xr = 0.12
+a0_eDep_yr = 0.55
 a0_eDep_theta = 0.
-a0_eDep_lr = 0.13
-a0_eDep_wr = 0.11
+a0_eDep_lr = 0.17
+a0_eDep_wr = 0.25
 a1_excitation_xr = 0.41
 a1_excitation_yr = 0.33
 a1_excitation_theta = 0.46
@@ -31,10 +39,10 @@ a2_ionization_xr = 0.41
 a2_ionization_yr = 0.53
 a2_ionization_theta = -0.67
 a2_ionization_lr = 0.11
-a3_heat_xr = 0.30
-a3_heat_yr = 0.67
+a3_heat_xr = 0.32
+a3_heat_yr = 0.77
 a3_heat_theta = -1 * m.pi / 2
-a3_heat_lr = 0.07
+a3_heat_lr = 0.08
 a4_recombination_xr = 0.55
 a4_recombination_yr = 0.50
 a4_recombination_theta = m.pi / 2
@@ -48,11 +56,11 @@ a6_tslow_yr = 0.29
 a6_tslow_theta = 0.
 a6_tslow_lr = 0.20
 a7_extraction_xr = 0.66
-a7_extraction_yr = 0.71
+a7_extraction_yr = 0.92
 a7_extraction_theta = 0.
 a7_extraction_lr = 0.07
 a8_s2_xr = 0.84
-a8_s2_yr = 0.71
+a8_s2_yr = 0.92
 a8_s2_theta = 0.
 a8_s2_lr = 0.07
 
@@ -67,14 +75,119 @@ arrow_properties['lr'] = np.array([a0_eDep_lr, a1_excitation_lr, a2_ionization_l
 arrow_properties['l'] = arrow_properties['lr'] * axes_xmax
 arrow_properties['wr'] = np.array([a0_eDep_wr, 0., 0., 0., 0., 0., 0., 0., 0.])
 arrow_properties['w'] = arrow_properties['wr'] * np.abs(axes_ymin)
+arrow_properties['color'] = np.full(len(arrow_properties['name']), '#000000')
 
 # Arrow head properties
 arrow_properties['head_hr'] = np.full(len(arrow_properties['name']), 0.04)
 arrow_properties['head_h'] = arrow_properties['head_hr'] * axes_xmax
 arrow_properties['min_head_br'] = np.full(len(arrow_properties['name']), 0.04)
 arrow_properties['min_head_b'] = arrow_properties['min_head_br'] * axes_xmax
+arrow_properties['head_b_scale'] = np.full(len(arrow_properties['name']), 1.25)
 
-# [0.13827839 0.40567766 0.40934066 0.30494505 0.55494505 0.75824176 0.75824176 0.66117216 0.83516484]
-# [0.49102873 0.33115891 0.5285492  0.67047445 0.49755403 0.1517132 0.28874447 0.70636359 0.70636359]
-# 0.4597565780541614
-# -0.6738629793235875
+# Atom position and size
+atom_left_posr = 0.17
+atom_bottom_posr = 0.36
+atom_widthr = 0.21
+atom_heightr = 0.21
+
+# Base (i.e. max) fontsize
+title_fontsize = fig_width / 16. * 35.
+
+# Text properties: text, position, halignment, fontsize, bolding, color
+# Convention is same as arrow properties; see above
+t0_pid_text = '%s Recoil (%s)'
+t0_pid_xr = 0.015
+t0_pid_yr = 0.06
+t0_pid_halign = 'left'
+t0_pid_fontsize = title_fontsize
+t0_pid_bold = True
+t1_eDep_text = 'Energy Deposition'
+t1_eDep_xr = 0.015
+t1_eDep_yr = 0.2
+t1_eDep_halign = 'left'
+t1_eDep_fontsize = title_fontsize * 0.85
+t1_eDep_bold = True
+t2_keV_text = '%.0f keV'
+t2_keV_xr = 0.18
+t2_keV_yr = 0.28
+t2_keV_halign = 'right'
+t2_keV_fontsize = title_fontsize * 0.85
+t2_keV_bold = True
+t3_field_text = '%.0f V/cm'
+t3_field_xr = 0.20
+t3_field_yr = 0.36
+t3_field_halign = 'right'
+t3_field_fontsize = title_fontsize * 0.85
+t3_field_bold = True
+t4_atom_text = 'Xe'
+t4_atom_xr = 0.33
+t4_atom_yr = 0.4
+t4_atom_halign = 'center'
+t4_atom_fontsize = title_fontsize * 1.2
+t4_atom_bold = True
+t5_heat_text = 'Heat (not observed)'
+t5_heat_xr = 0.32
+t5_heat_yr = 0.985
+t5_heat_halign = 'center'
+t5_heat_fontsize = title_fontsize * 0.85
+t5_heat_bold = False
+t6_excimer_text = r'Xe$_2^{*}$'
+t6_excimer_xr = 0.3
+t6_excimer_yr = 0.1
+t6_excimer_halign = 'center'
+t6_excimer_fontsize = title_fontsize
+t6_excimer_bold = True
+t7_eionpair_text = r'Xe$^{+}$/e$^{-}$'
+t7_eionpair_xr = 0.3
+t7_eionpair_yr = 0.9
+t7_eionpair_halign = 'center'
+t7_eionpair_fontsize = title_fontsize
+t7_eionpair_bold = True
+t8_tfast_text = r'$\tau_{fast}$'
+t8_tfast_xr = 0.7
+t8_tfast_yr = 0.1
+t8_tfast_halign = 'center'
+t8_tfast_fontsize = title_fontsize
+t8_tfast_bold = True
+t9_tslow_text = r'$\tau_{slow}$'
+t9_tslow_xr = 0.7
+t9_tslow_yr = 0.2
+t9_tslow_halign = 'center'
+t9_tslow_fontsize = title_fontsize
+t9_tslow_bold = True
+t10_S1_text = 'S1'
+t10_S1_xr = 0.9
+t10_S1_yr = 0.1
+t10_S1_halign = 'center'
+t10_S1_fontsize = title_fontsize
+t10_S1_bold = True
+t11_recombination_text = 'Recombination'
+t11_recombination_xr = 0.6
+t11_recombination_yr = 0.5
+t11_recombination_halign = 'center'
+t11_recombination_fontsize = title_fontsize
+t11_recombination_bold = True
+t12_electron_text = r'e$^{-}$'
+t12_electron_xr = 0.7
+t12_electron_yr = 0.9
+t12_electron_halign = 'center'
+t12_electron_fontsize = title_fontsize
+t12_electron_bold = True
+t13_S2_text = 'S2'
+t13_S2_xr = 0.96
+t13_S2_yr = 0.94
+t13_S2_halign = 'center'
+t13_S2_fontsize = title_fontsize * 1.2
+t13_S2_bold = True
+
+text_properties = {}
+text_properties['name'] = ['pid', 'eDep', 'keV', 'field', 'atom', 'heat', 'excimer', 'eionpair', 'tfast', 'tslow', 'S1', 'recombination', 'electron', 'S2']
+text_properties['text'] = np.array([t0_pid_text, t1_eDep_text, t2_keV_text, t3_field_text, t4_atom_text, t5_heat_text, t6_excimer_text, t7_eionpair_text, t8_tfast_text, t9_tslow_text, t10_S1_text, t11_recombination_text, t12_electron_text, t13_S2_text])
+text_properties['xr'] = np.array([t0_pid_xr, t1_eDep_xr, t2_keV_xr, t3_field_xr, t4_atom_xr, t5_heat_xr, t6_excimer_xr, t7_eionpair_xr, t8_tfast_xr, t9_tslow_xr, t10_S1_xr, t11_recombination_xr, t12_electron_xr, t13_S2_xr])
+text_properties['x'] = text_properties['xr'] * axes_xmax
+text_properties['yr'] = np.array([t0_pid_yr, t1_eDep_yr, t2_keV_yr, t3_field_yr, t4_atom_yr, t5_heat_yr, t6_excimer_yr, t7_eionpair_yr, t8_tfast_yr, t9_tslow_yr, t10_S1_yr, t11_recombination_yr, t12_electron_yr, t13_S2_yr])
+text_properties['y'] = text_properties['yr'] * axes_ymin
+text_properties['halign'] = np.array([t0_pid_halign, t1_eDep_halign, t2_keV_halign, t3_field_halign, t4_atom_halign, t5_heat_halign, t6_excimer_halign, t7_eionpair_halign, t8_tfast_halign, t9_tslow_halign, t10_S1_halign, t11_recombination_halign, t12_electron_halign, t13_S2_halign])
+text_properties['fontsize'] = np.array([t0_pid_fontsize, t1_eDep_fontsize, t2_keV_fontsize, t3_field_fontsize, t4_atom_fontsize, t5_heat_fontsize, t6_excimer_fontsize, t7_eionpair_fontsize, t8_tfast_fontsize, t9_tslow_fontsize, t10_S1_fontsize, t11_recombination_fontsize, t12_electron_fontsize, t13_S2_fontsize])
+text_properties['bold'] = np.array([t0_pid_bold, t1_eDep_bold, t2_keV_bold, t3_field_bold, t4_atom_bold, t5_heat_bold, t6_excimer_bold, t7_eionpair_bold, t8_tfast_bold, t9_tslow_bold, t10_S1_bold, t11_recombination_bold, t12_electron_bold, t13_S2_bold])
+text_properties['color'] = np.full(len(text_properties['name']), '#000000')
--- XenimationHelper.py
+++ XenimationHelper.py
@@ -6,14 +6,69 @@ from PIL import Image
 import XenimationConfig as XC
 import DetectorConfig as DC
 import NESThelper as nest
+from XenimationHelper import *
 
 
 
-def getFlowImage(pid, eDep, field):
+def GetFlowImage(pid, eDep, field):
     
     fig, ax = plt.subplots(1, 1, figsize=(XC.fig_width, XC.fig_height))
+    ax.set_xlim([0, XC.axes_xmax])
+    ax.set_ylim([XC.axes_ymin, 0])
+    
+    SetColors(pid)
+    SetText(pid, eDep, field)
 
     Ni, Nex, Nph, Ne = nest.GetYieldNR(eDep, field, DC.Density)
+    SetArrowWidths(pid, Ni, Nex, Nph, Ne)
+    
+    for a in range(len(XC.arrow_properties['name'])):
+        DrawArrow(fig, ax, a)
+    
+    #for t in range(len(XC.text_properties['name'])):
+    for t in [0, 1, 2, 3, 4, 5, 13]:
+        DrawText(fig, ax, t)
+
+    DrawAtom(fig, ax, pid)
+
+    ax.set_xlim([0, XC.axes_xmax])
+    ax.set_ylim([XC.axes_ymin, 0])
+    ax.axis('off')
+    fig.tight_layout()
+    fig.savefig('test.png', transparent=False)
+
+
+
+def SetColors(pid):
+    
+    XC.arrow_properties['color'][0] = XC.EDep_color
+    XC.text_properties['color'][1] = XC.EDep_color
+    XC.text_properties['color'][2] = XC.EDep_color
+    XC.text_properties['color'][3] = XC.EDep_color
+    for a in range(1, len(XC.arrow_properties['color'])):
+        if (pid == 'ER'):
+            XC.arrow_properties['color'][a] = XC.ER_color
+            XC.text_properties['color'][0] = XC.ER_color
+        elif (pid == 'NR'):
+            XC.arrow_properties['color'][a] = XC.NR_color
+            XC.text_properties['color'][0] = XC.NR_color
+
+
+
+def SetText(pid, eDep, field):
+    
+    if (pid == 'ER'):
+        XC.text_properties['text'][0] = XC.text_properties['text'][0] % ('Electronic', 'ER')
+    elif (pid == 'NR'):
+        XC.text_properties['text'][0] = XC.text_properties['text'][0] % ('Nuclear', 'NR')
+
+    XC.text_properties['text'][2] = XC.text_properties['text'][2] % eDep
+    XC.text_properties['text'][3] = XC.text_properties['text'][3] % field
+
+
+
+def SetArrowWidths(pid, Ni, Nex, Nph, Ne):
+    
     XC.arrow_properties['w'][1] = XC.arrow_properties['w'][0] * Nex / (Nex + Ni) * (1 - XC.NR_heat_fraction)
     XC.arrow_properties['w'][2] = XC.arrow_properties['w'][0] * Ni / (Nex + Ni) * (1 - XC.NR_heat_fraction)
     XC.arrow_properties['w'][3] = XC.arrow_properties['w'][0] * XC.NR_heat_fraction
@@ -22,101 +77,112 @@ def getFlowImage(pid, eDep, field):
     XC.arrow_properties['w'][6] = Nph / Nex * XC.arrow_properties['w'][1] * 0.95
     XC.arrow_properties['w'][7] = Ne / Ni * XC.arrow_properties['w'][2]
     XC.arrow_properties['w'][8] = Ne / Ni * XC.arrow_properties['w'][2]
-    XC.arrow_properties['head_b'] = np.maximum(XC.arrow_properties['w'] * 1.25, XC.arrow_properties['min_head_b'])
+    XC.arrow_properties['head_b'] = np.maximum(XC.arrow_properties['w'] * XC.arrow_properties['head_b_scale'],
+        XC.arrow_properties['min_head_b'])
+
+
+
+def DrawArrow(fig, ax, arrow_id):
     
-    for a in range(len(XC.arrow_properties['name'])):
+    a = arrow_id
 
-        # Draw the four lines corresponding to the main rectangular box of the arrow
-        # The lines are, in order: top, right, bottom, left, in unrotated space
-        npts = 200
-        box0_x_unrot = np.linspace(-1 * XC.arrow_properties['l'][a] / 2, XC.arrow_properties['l'][a] / 2, npts)
-        box0_y_unrot = np.full(npts, XC.arrow_properties['w'][a] / 2)
-        box0_x, box0_y = rotate(box0_x_unrot, box0_y_unrot, XC.arrow_properties['theta'][a])
-        box0_x, box0_y = shift(box0_x, box0_y, XC.arrow_properties['x'][a], XC.arrow_properties['y'][a])
-        box0_x, box0_y = getXIncreasing(box0_x, box0_y)
-        box1_x_unrot = np.full(npts, XC.arrow_properties['l'][a] / 2)
-        box1_y_unrot = np.linspace(-1 * XC.arrow_properties['w'][a] / 2, XC.arrow_properties['w'][a] / 2, npts)
-        box1_x, box1_y = rotate(box1_x_unrot, box1_y_unrot, XC.arrow_properties['theta'][a])
-        box1_x, box1_y = shift(box1_x, box1_y, XC.arrow_properties['x'][a], XC.arrow_properties['y'][a])
-        box1_x, box1_y = getXIncreasing(box1_x, box1_y)
-        box2_x_unrot = np.linspace(-1 * XC.arrow_properties['l'][a] / 2, XC.arrow_properties['l'][a] / 2, npts)
-        box2_y_unrot = np.full(npts, -1 * XC.arrow_properties['w'][a] / 2)
-        box2_x, box2_y = rotate(box2_x_unrot, box2_y_unrot, XC.arrow_properties['theta'][a])
-        box2_x, box2_y = shift(box2_x, box2_y, XC.arrow_properties['x'][a], XC.arrow_properties['y'][a])
-        box2_x, box2_y = getXIncreasing(box2_x, box2_y)
-        box3_x_unrot = np.full(npts, -1 * XC.arrow_properties['l'][a] / 2)
-        box3_y_unrot = np.linspace(-1 * XC.arrow_properties['w'][a] / 2, XC.arrow_properties['w'][a] / 2, npts)
-        box3_x, box3_y = rotate(box3_x_unrot, box3_y_unrot, XC.arrow_properties['theta'][a])
-        box3_x, box3_y = shift(box3_x, box3_y, XC.arrow_properties['x'][a], XC.arrow_properties['y'][a])
-        box3_x, box3_y = getXIncreasing(box3_x, box3_y)
-        
-        # Draw the three lines corresponding to the arrow head
-        # The lines are, in order: base, top diagonal, bottom diagonal (imagine arrow pointing right)
-        head0_x_unrot = np.full(npts, XC.arrow_properties['l'][a] / 2)
-        head0_y_unrot = np.linspace(-1 * XC.arrow_properties['head_b'][a] / 2, XC.arrow_properties['head_b'][a] / 2, npts)
-        head0_x, head0_y = rotate(head0_x_unrot, head0_y_unrot, XC.arrow_properties['theta'][a])
-        head0_x, head0_y = shift(head0_x, head0_y, XC.arrow_properties['x'][a], XC.arrow_properties['y'][a])
-        head0_x, head0_y = getXIncreasing(head0_x, head0_y)
-        head1_x_unrot = np.linspace(XC.arrow_properties['l'][a] / 2, XC.arrow_properties['l'][a] / 2 + XC.arrow_properties['head_h'][a], npts)
-        head1_y_unrot = np.linspace(XC.arrow_properties['head_b'][a] / 2, 0, npts)
-        head1_x, head1_y = rotate(head1_x_unrot, head1_y_unrot, XC.arrow_properties['theta'][a])
-        head1_x, head1_y = shift(head1_x, head1_y, XC.arrow_properties['x'][a], XC.arrow_properties['y'][a])
-        head1_x, head1_y = getXIncreasing(head1_x, head1_y)
-        head2_x_unrot = np.linspace(XC.arrow_properties['l'][a] / 2, XC.arrow_properties['l'][a] / 2 + XC.arrow_properties['head_h'][a], npts)
-        head2_y_unrot = np.linspace(-1 * XC.arrow_properties['head_b'][a] / 2, 0, npts)
-        head2_x, head2_y = rotate(head2_x_unrot, head2_y_unrot, XC.arrow_properties['theta'][a])
-        head2_x, head2_y = shift(head2_x, head2_y, XC.arrow_properties['x'][a], XC.arrow_properties['y'][a])
-        head2_x, head2_y = getXIncreasing(head2_x, head2_y)
-                
-        # Get the maximum and minimum boundaries for the arrow box
-        box_x = np.linspace(min(min(box0_x), min(box1_x), min(box2_x), min(box3_x)),
-            max(max(box0_x), max(box1_x), max(box2_x), max(box3_x)), npts * 2)
-        box_y_lo = np.zeros(len(box_x))
-        box_y_hi = np.zeros(len(box_x))
-        box0_y_int = np.interp(box_x, box0_x, box0_y, left=np.nan, right=np.nan)
-        box1_y_int = np.interp(box_x, box1_x, box1_y, left=np.nan, right=np.nan)
-        box2_y_int = np.interp(box_x, box2_x, box2_y, left=np.nan, right=np.nan)
-        box3_y_int = np.interp(box_x, box3_x, box3_y, left=np.nan, right=np.nan)
-        for p in range(len(box_x)):
-            box_y_lo[p] = np.nanmin([box0_y_int[p], box1_y_int[p], box2_y_int[p], box3_y_int[p]])
-            box_y_hi[p] = np.nanmax([box0_y_int[p], box1_y_int[p], box2_y_int[p], box3_y_int[p]])
+    # Draw the four lines corresponding to the main rectangular box of the arrow
+    # The lines are, in order: top, right, bottom, left, in unrotated space
+    npts = 200
+    box0_x_unrot = np.linspace(-1 * XC.arrow_properties['l'][a] / 2, XC.arrow_properties['l'][a] / 2, npts)
+    box0_y_unrot = np.full(npts, XC.arrow_properties['w'][a] / 2)
+    box0_x, box0_y = Rotate(box0_x_unrot, box0_y_unrot, XC.arrow_properties['theta'][a])
+    box0_x, box0_y = Shift(box0_x, box0_y, XC.arrow_properties['x'][a], XC.arrow_properties['y'][a])
+    box0_x, box0_y = GetXIncreasing(box0_x, box0_y)
+    box1_x_unrot = np.full(npts, XC.arrow_properties['l'][a] / 2)
+    box1_y_unrot = np.linspace(-1 * XC.arrow_properties['w'][a] / 2, XC.arrow_properties['w'][a] / 2, npts)
+    box1_x, box1_y = Rotate(box1_x_unrot, box1_y_unrot, XC.arrow_properties['theta'][a])
+    box1_x, box1_y = Shift(box1_x, box1_y, XC.arrow_properties['x'][a], XC.arrow_properties['y'][a])
+    box1_x, box1_y = GetXIncreasing(box1_x, box1_y)
+    box2_x_unrot = np.linspace(-1 * XC.arrow_properties['l'][a] / 2, XC.arrow_properties['l'][a] / 2, npts)
+    box2_y_unrot = np.full(npts, -1 * XC.arrow_properties['w'][a] / 2)
+    box2_x, box2_y = Rotate(box2_x_unrot, box2_y_unrot, XC.arrow_properties['theta'][a])
+    box2_x, box2_y = Shift(box2_x, box2_y, XC.arrow_properties['x'][a], XC.arrow_properties['y'][a])
+    box2_x, box2_y = GetXIncreasing(box2_x, box2_y)
+    box3_x_unrot = np.full(npts, -1 * XC.arrow_properties['l'][a] / 2)
+    box3_y_unrot = np.linspace(-1 * XC.arrow_properties['w'][a] / 2, XC.arrow_properties['w'][a] / 2, npts)
+    box3_x, box3_y = Rotate(box3_x_unrot, box3_y_unrot, XC.arrow_properties['theta'][a])
+    box3_x, box3_y = Shift(box3_x, box3_y, XC.arrow_properties['x'][a], XC.arrow_properties['y'][a])
+    box3_x, box3_y = GetXIncreasing(box3_x, box3_y)
 
-        # Get the maximum and minimum boundaries for the arrow head
-        head_x = np.linspace(min(min(head0_x), min(head1_x), min(head2_x)),
-            max(max(head0_x), max(head1_x), max(head2_x)), npts * 2)
-        head_y_lo = np.zeros(len(head_x))
-        head_y_hi = np.zeros(len(head_x))
-        head0_y_int = np.interp(head_x, head0_x, head0_y, left=np.nan, right=np.nan)
-        head1_y_int = np.interp(head_x, head1_x, head1_y, left=np.nan, right=np.nan)
-        head2_y_int = np.interp(head_x, head2_x, head2_y, left=np.nan, right=np.nan)
-        for p in range(len(head_x)):
-            head_y_lo[p] = np.nanmin([head0_y_int[p], head1_y_int[p], head2_y_int[p]])
-            head_y_hi[p] = np.nanmax([head0_y_int[p], head1_y_int[p], head2_y_int[p]])
+    # Draw the three lines corresponding to the arrow head
+    # The lines are, in order: base, top diagonal, bottom diagonal (imagine arrow pointing right)
+    head0_x_unrot = np.full(npts, XC.arrow_properties['l'][a] / 2)
+    head0_y_unrot = np.linspace(-1 * XC.arrow_properties['head_b'][a] / 2, XC.arrow_properties['head_b'][a] / 2, npts)
+    head0_x, head0_y = Rotate(head0_x_unrot, head0_y_unrot, XC.arrow_properties['theta'][a])
+    head0_x, head0_y = Shift(head0_x, head0_y, XC.arrow_properties['x'][a], XC.arrow_properties['y'][a])
+    head0_x, head0_y = GetXIncreasing(head0_x, head0_y)
+    head1_x_unrot = np.linspace(XC.arrow_properties['l'][a] / 2, XC.arrow_properties['l'][a] / 2 + XC.arrow_properties['head_h'][a], npts)
+    head1_y_unrot = np.linspace(XC.arrow_properties['head_b'][a] / 2, 0, npts)
+    head1_x, head1_y = Rotate(head1_x_unrot, head1_y_unrot, XC.arrow_properties['theta'][a])
+    head1_x, head1_y = Shift(head1_x, head1_y, XC.arrow_properties['x'][a], XC.arrow_properties['y'][a])
+    head1_x, head1_y = GetXIncreasing(head1_x, head1_y)
+    head2_x_unrot = np.linspace(XC.arrow_properties['l'][a] / 2, XC.arrow_properties['l'][a] / 2 + XC.arrow_properties['head_h'][a], npts)
+    head2_y_unrot = np.linspace(-1 * XC.arrow_properties['head_b'][a] / 2, 0, npts)
+    head2_x, head2_y = Rotate(head2_x_unrot, head2_y_unrot, XC.arrow_properties['theta'][a])
+    head2_x, head2_y = Shift(head2_x, head2_y, XC.arrow_properties['x'][a], XC.arrow_properties['y'][a])
+    head2_x, head2_y = GetXIncreasing(head2_x, head2_y)
 
-        # Plot arrow
-        if (a == 0):
-            ax.fill_between(box_x, box_y_lo, box_y_hi, color=XC.EDep_color)
-            ax.fill_between(head_x, head_y_lo, head_y_hi, color=XC.EDep_color)
-        else:
-            ax.fill_between(box_x, box_y_lo, box_y_hi, color=XC.NR_color)
-            ax.fill_between(head_x, head_y_lo, head_y_hi, color=XC.NR_color)
+    # Get the maximum and minimum boundaries for the arrow box
+    box_x = np.linspace(min(min(box0_x), min(box1_x), min(box2_x), min(box3_x)),
+        max(max(box0_x), max(box1_x), max(box2_x), max(box3_x)), npts * 2)
+    box_y_lo = np.zeros(len(box_x))
+    box_y_hi = np.zeros(len(box_x))
+    box0_y_int = np.interp(box_x, box0_x, box0_y, left=np.nan, right=np.nan)
+    box1_y_int = np.interp(box_x, box1_x, box1_y, left=np.nan, right=np.nan)
+    box2_y_int = np.interp(box_x, box2_x, box2_y, left=np.nan, right=np.nan)
+    box3_y_int = np.interp(box_x, box3_x, box3_y, left=np.nan, right=np.nan)
+    for p in range(len(box_x)):
+        box_y_lo[p] = np.nanmin([box0_y_int[p], box1_y_int[p], box2_y_int[p], box3_y_int[p]])
+        box_y_hi[p] = np.nanmax([box0_y_int[p], box1_y_int[p], box2_y_int[p], box3_y_int[p]])
 
+    # Get the maximum and minimum boundaries for the arrow head
+    head_x = np.linspace(min(min(head0_x), min(head1_x), min(head2_x)),
+        max(max(head0_x), max(head1_x), max(head2_x)), npts * 2)
+    head_y_lo = np.zeros(len(head_x))
+    head_y_hi = np.zeros(len(head_x))
+    head0_y_int = np.interp(head_x, head0_x, head0_y, left=np.nan, right=np.nan)
+    head1_y_int = np.interp(head_x, head1_x, head1_y, left=np.nan, right=np.nan)
+    head2_y_int = np.interp(head_x, head2_x, head2_y, left=np.nan, right=np.nan)
+    for p in range(len(head_x)):
+        head_y_lo[p] = np.nanmin([head0_y_int[p], head1_y_int[p], head2_y_int[p]])
+        head_y_hi[p] = np.nanmax([head0_y_int[p], head1_y_int[p], head2_y_int[p]])
+
+    # Plot arrow
+    ax.fill_between(box_x, box_y_lo, box_y_hi, color=XC.arrow_properties['color'][a])
+    ax.fill_between(head_x, head_y_lo, head_y_hi, color=XC.arrow_properties['color'][a])
+
+
+
+def DrawText(fig, ax, text_id):
+    
+    t = text_id
+    ax.text(XC.text_properties['x'][t], XC.text_properties['y'][t], XC.text_properties['text'][t],
+        fontsize=XC.text_properties['fontsize'][t], color=XC.text_properties['color'][t],
+        ha=XC.text_properties['halign'][t],
+        fontweight=('bold' * XC.text_properties['bold'][t] + 'normal' * (1 - XC.text_properties['bold'][t])))
+
+
+
+def DrawAtom(fig, ax, pid):
+    
     if (pid == 'NR'):
         im = plt.imread('Atom_NR.png')
     elif (pid == 'ER'):
         im = plt.imread('Atom_ER.png')
-    newax = fig.add_axes([0.2, 0.4, 0.1, 0.1], anchor='NE', zorder=-1)
-    newax.imshow(im)
-    newax.axis('off')
-
-    ax.axis('off')
-    fig.tight_layout()
-    fig.savefig('test.png')
+    imax = fig.add_axes([XC.atom_left_posr, XC.atom_bottom_posr, XC.atom_widthr, XC.atom_heightr], anchor='NE', zorder=-1)
+    imax.imshow(im)
+    imax.axis('off')
 
 
 
 ### Rotate a vector (x, y) by an angle theta [in radians]
-def rotate(x, y, theta):
+def Rotate(x, y, theta):
     
     xnew = x * np.cos(theta) - y * np.sin(theta)
     ynew = x * np.sin(theta) + y * np.cos(theta)
@@ -125,8 +191,8 @@ def rotate(x, y, theta):
 
 
 
-### Shift a vector (x,y) by a constant (shift_x, shift_y)
-def shift(x, y, shift_x, shift_y):
+### Shift a vector (x,y) by a constant (Shift_x, Shift_y)
+def Shift(x, y, shift_x, shift_y):
     
     return (x + shift_x), (y + shift_y)
 
@@ -137,7 +203,7 @@ def shift(x, y, shift_x, shift_y):
 ### Returns: if x is monotonically increasing, return x and y unchanged
 ###          if x is monotonically decreasing, return x and y in reverse order
 ###          if x is not monotonically changing, return x and y changed; print an error message
-def getXIncreasing(x, y):
+def GetXIncreasing(x, y):
     
     # Get sorting order of x. If sort = 0, it is unsorted. If sort = 1, it is forward sorted.
     # If sort = 2, it is reverse sorted.
@@ -156,7 +222,7 @@ def getXIncreasing(x, y):
             break
 
     if sort == 0:
-        print('Warning in getXIncreasing() in XenimationHelper.py; array is not monotonic')
+        print('Warning in GetXIncreasing() in XenimationHelper.py; array is not monotonic')
         return x, y
     elif sort == 1:
         return x, y
